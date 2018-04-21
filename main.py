7344da06cc79e609b85af7fb6bcdd6c113e3b117
rosborne <rosborne255@yahoo.com>
2018-04-21 13:14:24 -0500

    Initial project checkin

---
+++ main.py
@@ -0,0 +1,56 @@
+import queue
+
+class TreeNode:
+    left = None
+    right = None
+
+    def __init__(self, number):
+        self.value = number
+
+    def printInOrder(self):
+        if (self.left):
+            self.left.printInOrder()
+        print(self.value)
+        if (self.right):
+            self.right.printInOrder()
+
+    def printPreOrder(self):
+        print(self.value)
+        if (self.left):
+            self.left.printPreOrder()        
+        if (self.right):
+            self.right.printPreOrder()
+
+    def printPostOrder(self):        
+        if (self.left):
+            self.left.printPostOrder()        
+        if (self.right):
+            self.right.printPostOrder()
+        print(self.value)
+
+    def printLevelOrder(self):
+        q = queue.Queue()
+        q.put(self)
+        while not q.empty():
+            node = q.get()
+            print(node.value)
+            if (node.left):
+                q.put(node.left)
+            if (node.right):
+                q.put(node.right)   
+
+node = TreeNode(1)
+node.left = TreeNode(2)
+node.right = TreeNode(3)
+node.left.left = TreeNode(4)
+node.left.right = TreeNode(5)
+
+print ("Print Tree InOrder")
+node.printInOrder()
+print ("Print Tree PreOrder")
+node.printPreOrder()
+print ("Print Tree PostOrder")
+node.printPostOrder()
+
+print ("Print Tree Level Order")
+node.printLevelOrder()
---
+++ readme.md
@@ -0,0 +1,24 @@
+This is your basic TreeNode implementation. I enjoy working with TreeNode data structures so figured it would be a good idea to put up a quick example.
+
+Below is the command you should use to execute this program:
+
+```bash
+python main.py
+```
+
+This script will build a tree and run the typical traversal techniques: inorder, postorder, preorder, & level order.
+
+Look at the following image of the tree we are building:
+
+![alt text](level_order_traversal.png "Tree Node Graph")
+
+The Output from the Program is expected to be the following:
+Depth First Traversals:
+
+(a) Inorder (Left, Root, Right) : 4 2 5 1 3
+
+(b) Preorder (Root, Left, Right) : 1 2 4 5 3
+
+(c) Postorder (Left, Right, Root) : 4 5 2 3 1
+
+Breadth First or Level Order Traversal : 1 2 3 4 5
